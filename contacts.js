b3c93949c3ff42a60176497c8258076d8560a6d0
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-02 23:16:21 +0300

    Add updateContact function. Add invokaAction

--- contacts.js
+++ contacts.js
@@ -1,10 +1,10 @@
 const fs = require("fs/promises");
 const path = require("path");
-const { nanoid } = require("nanoid"); //? npm i nanoid@3 -D (it's mean i install nanoid version 3 (not current, 4))
+const { nanoid } = require("nanoid"); //? npm i nanoid@3 -D (it's mean i install nanoid version 3 not current - 4)
 
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-async function listContacts() {
+async function getAllContacts() {
   //? how filepath works :
   //? __dirname is the current directory
   //? +
@@ -12,24 +12,41 @@ async function listContacts() {
 
   const data = await fs.readFile(contactsPath, "utf8");
 
-  console.log("typeof data ===>", typeof data); // it's an object
+  console.log("typeof data ===>", typeof JSON.parse(data)); // it's an object
   return JSON.parse(data);
 }
 
 async function getContactById(contactID) {
-  const contacts = await listContacts();
+  const contacts = await getAllContacts();
 
   const contact = contacts.find((contact) => contact.id === contactID);
 
   return contact;
 }
 
-async function updateContacts(contacts) {
-  return fs.appendFile(contactsPath, JSON.stringify(contacts));
+async function updateContact(id, updatedData) {
+  try {
+    const data = await fs.readFile("contacts.json", "utf-8");
+
+    const contacts = JSON.parse(data);
+
+    const index = contacts.findIndex((contact) => contact.id === id);
+    if (index !== -1) {
+      contacts[index] = { ...contacts[index], ...updatedData };
+    } else {
+      throw new Error(`Contact with id ${id} not found`);
+    }
+
+    await fs.writeFile("contacts.json", JSON.stringify(contacts, null, 2));
+
+    console.log(`Contact with id ${id} updated successfully`);
+  } catch (error) {
+    console.error("Failed to update contact:", error);
+  }
 }
 
 async function addContact(name, email, phone) {
-  const contacts = await listContacts();
+  const contacts = await getAllContacts();
 
   const newContact = { name, email, phone, id: nanoid(10) };
 
@@ -41,7 +58,7 @@ async function addContact(name, email, phone) {
 }
 
 async function removeContact(contactID) {
-  const contacts = await listContacts();
+  const contacts = await getAllContacts();
   const index = contacts.findIndex((contact) => contact.id === contactID);
 
   if (index === -1) throw new Error("Contact not found");
@@ -49,4 +66,10 @@ async function removeContact(contactID) {
   contacts.splice(index, 1);
 }
 
-module.exports = { listContacts, addContact, getContactById, removeContact };
+module.exports = {
+  getAllContacts,
+  addContact,
+  updateContact,
+  getContactById,
+  removeContact,
+};
--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-const { listContacts, addContact, getContactById } = require("./contacts");
+// import chalk from "chalk";
+const contacts = require("./contacts");
 
-listContacts().then(console.log).catch(console.error);
+contacts.getAllContacts().then(console.log).catch(console.error);
 
 // getContactById("qdggE76Jtbfd9eWJHrssH").then(console.log).catch(console.error);
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "getAll":
+      const allContacts = contacts.getAllContacts;
+      console.log(allContacts);
+
+      break;
+
+    case "getById":
+      const contact = await contacts.getById(id);
+      console.log(contact);
+
+      break;
+
+    case "addContact":
+      const newContact = await contacts.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.log(newContact);
+
+      break;
+
+    case "updateContact":
+      const updatedContact = await contacts.updateContact(id, {
+        name,
+        email,
+        phone,
+      });
+
+      break;
+
+    default:
+      console.warn(chalk.red("Unknown action type!"));
+      break;
+  }
+}
